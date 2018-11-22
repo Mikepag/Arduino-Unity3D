3f451cf518cd0eccc2e3f864955e69a9ab9d21b5
Mikepag <p14pagk@ionio.gr>
2018-11-22 17:20:59 +0200

    Update cubeRotation.cs
    * Checking if value sent from Arduino refers to a left-to-right or right-to-left hand gesture.
    * Rotating cube left or right accordingly.
    * Now the cube is rotated considering the speed of the hand gesture.

--- cubeRotation.cs
+++ cubeRotation.cs
@@ -9,7 +9,7 @@ public class cubeRotation: MonoBehaviour {
 	//Recieving data from the arduino:
 	SerialPort sp = new SerialPort("COM3", 9600);
 
-	public int direction;
+	public int directionSteps;
 	public int timesteps;
 
 
@@ -25,32 +25,34 @@ public class cubeRotation: MonoBehaviour {
 	void Update ()
 	{
 		if (sp.IsOpen) {
-			direction = sp.ReadByte ();
+			directionSteps = sp.ReadByte ();
 			//RotateObject (direction);
 
 			//StopCoroutine (RotateObject(direction));
-			StartCoroutine (RotateObject(direction));
+			StartCoroutine (RotateObject(directionSteps));
 		}
 	}
+		
 
 
+	IEnumerator RotateObject (int directionSteps){
+		if (directionSteps >0 && directionSteps <= 10) {
+			directionSteps = (10 - directionSteps) * 10;
 
-	IEnumerator RotateObject (int direction){
-		if (direction == 0) {
 			timesteps = 0;
 			while (timesteps < 10) {
-				transform.Rotate (Vector3.up * 15 * Time.deltaTime);
-				yield return new WaitForSeconds (0.001F);	// Suspends the coroutine execution for the given amount of seconds using scaled time.
+				transform.Rotate (Vector3.up * directionSteps * Time.deltaTime);
+				yield return new WaitForSeconds (0.0001F);	// Suspends the coroutine execution for the given amount of seconds using scaled time.
 				timesteps++;
 			}
-		} else if (direction == 1) {
+		} else if (directionSteps >10) {
+			directionSteps = (10 -(directionSteps - 10)) * 10;
 			timesteps = 0;
 			while (timesteps < 10) {
-				transform.Rotate (Vector3.down * 15 * Time.deltaTime);
-				yield return new WaitForSeconds (0.001F);	//Suspends the coroutine execution for the given amount of seconds using scaled time.
+				transform.Rotate (Vector3.down * directionSteps * Time.deltaTime);
+				yield return new WaitForSeconds (0.0001F);	//Suspends the coroutine execution for the given amount of seconds using scaled time.
 				timesteps++;
 			}
 		}
 	}
-
 }
