eaf81efb3f2004e80ec48cf157b300fbe56c48f7
Mikepag <p14pagk@ionio.gr>
2019-02-21 04:08:11 +0200

    Update timer.cs
    * Created another script (restart.cs) and a restart button.
    * The new script detects when the restart button is clicked.
    * I get the value of the the new script's variable "toRestart".
    * If toRestart == 1, it means that the button was clicked and I need to start a countdown and restart the timer (and everything else).
    
    ___
    Something's wrong with the code. In the last 6 hours I'm debugging it using Breakpoints on Visual Studio but I didn't make much progress.

--- timer.cs
+++ timer.cs
@@ -7,6 +7,7 @@ using System.IO;
 public class timer : MonoBehaviour {
 
     public GameObject Compass;
+    public int unfinCD = 0;   // Finished Countdown
 
     public Text timerText;      // Declaring a public Text called timerText
     private float startTime;    // 
@@ -15,61 +16,146 @@ public class timer : MonoBehaviour {
     private int milisecInt;
     private int stopTime;
     private int updatedFile;    // ==1 when the time has already been writen to test.txt, ==0 when it need to be written.
+    private int toCountdown;
+    private int didCntdown;  // ==1 when I already counted down, ==0 at the start of the game, when I need to do a countdown.
+    private float passedTime;
 
     private string filename = "test.txt";
     private string textToWrite = "Time: ";
 
 
+
     // Use this for initialization
-    void Start () {
-        startTime = Time.time;  // Time.time gives us the time since the application started.
+    void Start() {
+        //startTime = Time.time;  // Time.time gives us the time since the application started.
+        stopTime = 0;
+        toCountdown = 0;
+        secondsInt = 0;
+        didCntdown = 0;
 
         updatedFile = 0;
         if (File.Exists(filename))  // If test.txt already exists...
         {
             File.Delete(filename);  // ...I delete it so that all the previous data stored is deleted. The file is created again when I send data to it, but it is empty.
         }
+
+        //Countdown(startTime);
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        stopTime = Compass.GetComponent<compassRotation>().stopRotating;
-
-        if (stopTime == 1){
+    // Update is called once per frame
+    void Update() {
+        stopTime = Compass.GetComponent<compassRotation>().stopRotating;    // Getting the value of stopRotating from the compassRotation script.
+        if (stopTime == 1) {
             timerText.color = Color.red;
 
-            if (updatedFile == 0)
-            {
-                textToWrite = textToWrite + " " + timerText.text.ToString() + "\n";    //create a proper string.
-                File.AppendAllText(filename, textToWrite);  //write to the file.
+            if (updatedFile == 0) {   // If I haven't printed the data to the file yet...
+                textToWrite = textToWrite + " " + timerText.text.ToString() + "\n";    // Create a proper string of the time's value.
+                File.AppendAllText(filename, textToWrite);  // Print it to the file.
                 updatedFile = 1;
             }
-
             return;
         }
 
-        float t = Time.time - startTime;  // Gives the time in seconds since timer started.
-
-        string minutes = ((int)t / 60).ToString();  // gives number of minutes by dividing the integer value of t by 60.
-        //string seconds = (t % 60).ToString("f2");   // f2 is there to keep only 2 decimal places.
-        string seconds = ((int)t % 60).ToString();
-        string miliseconds = ((int)(t * 100f) % 100).ToString();
-
-        minutesInt = ((int)t / 60);
-        secondsInt = ((int)t % 60);
-        milisecInt = ((int)(t * 100f) % 100);
-
-        if (minutesInt < 10){
-            minutes = "0" + minutes;
+        toCountdown = Compass.GetComponent<restart>().toRestart;
+        if (toCountdown == 1)
+        {
+            //startTime = Time.time;
+            secondsInt = 0;
+            while (secondsInt <= 3)
+            {
+                //passedTime = Time.time - startTime;  // Gives the time in seconds since timer started.
+                //secondsInt = ((int)passedTime % 60);
+                
+                if (secondsInt == 0)
+                {
+                    timerText.text = "3";
+                    secondsInt = 1;
+                }
+                else if (secondsInt == 1)
+                {
+                    timerText.text = "2";
+                    secondsInt = 2;
+                }
+                else if (secondsInt == 2)
+                {
+                    timerText.text = "1";
+                    secondsInt = 3;
+                }
+                else
+                {
+                    timerText.text = "GO!";
+                    secondsInt = 4;
+                }
+                //yield return new WaitForSeconds(0.05F);    // Suspends the coroutine execution for the given amount of seconds using scaled time.
+                //secondsInt++;
+            }
+            unfinCD = 0;
+            didCntdown = 1;
+            toCountdown = 0;
+            startTime = Time.time;
         }
-        if(secondsInt < 10){
-            seconds = "0" + seconds;
-        }
-        if(milisecInt < 10){
-            miliseconds = "0" + miliseconds;
-        }
 
-        timerText.text = minutes + ":" + seconds + ":" + miliseconds;
-	}
+        if (didCntdown == 1)
+        {
+            passedTime = Time.time - startTime;  // Gives the time in seconds since timer started.
+
+            string minutes = ((int)passedTime / 60).ToString();  // gives number of minutes by dividing the integer value of t by 60.
+            string seconds = ((int)passedTime % 60).ToString();
+            string miliseconds = ((int)(passedTime * 100f) % 100).ToString();
+
+            minutesInt = ((int)passedTime / 60);
+            secondsInt = ((int)passedTime % 60);
+            milisecInt = ((int)(passedTime * 100f) % 100);
+
+            if (minutesInt < 10)
+            {
+                minutes = "0" + minutes;
+            }
+            if (secondsInt < 10)
+            {
+                seconds = "0" + seconds;
+            }
+            if (milisecInt < 10)
+            {
+                miliseconds = "0" + miliseconds;
+            }
+
+            timerText.text = minutes + ":" + seconds + ":" + miliseconds;
+        }
+        else
+        {
+            timerText.text = "Press The Fucking Button";
+        }
+    }
+    
+    
+
+
+	
 }
+
+/*
+public int Countdown(float startTime) {
+    float passedTime; 
+    int secondsInt = 0;
+
+    while(secondsInt <= 3)
+    {
+        passedTime = Time.time - startTime;  // Gives the time in seconds since timer started.
+        secondsInt = ((int)passedTime % 60);
+        if(secondsInt == 0) {
+            timerText.text = "3";
+        }
+        else if(secondsInt == 1) {
+            timerText.text = "2";
+        }
+        else if (secondsInt == 2) {
+            timerText.text = "1";
+        }
+        else {
+            timerText.text = "GO!";
+        }
+    }
+    return 1;    // == "didCntdown = 1;"
+}
+*/
