a4732666f45909bbd37ca525c5b780e58bf77219
Mikepag <p14pagk@ionio.gr>
2019-02-22 21:31:12 +0200

    Update restart.cs
    * I used to have restartButton defined only as a Button but because I wanted to be able to hide it completely from the screen once it has been clicked, I also defined it as a GameObject. (I wasn't able to hide it as a Button, only set "restartButton.enabled = false" or "restartButton.interactable = false" which wasn't what I wanted).
    * I set the restartButton visible (resButObject.SetActive(true)) at the Start(), hide it (resButObject.SetActive(false)) after it has been clicked and then set it visible again when the goal is achieved.

--- restart.cs
+++ restart.cs
@@ -6,22 +6,37 @@ using System.IO;
 
 public class restart : MonoBehaviour {
     public GameObject Compass;
-
-    public Button restartButton;
-    public int toRestart;
+    public GameObject resButObject; // I use this to hide the restart button when it is clicked.
+    public Button restartButton;    // I use this to know when the restart button gets clicked.
+    public int resBtnClicked;
 
     void Start()
     {
-        restartButton.onClick.AddListener(TaskOnClick);
+        restartButton.onClick.AddListener(TaskOnClick); // Whenever the restart button is clicked, call the TaskOnClick() function.
+        resButObject.SetActive(true);
     }
 
     void Update()
     {
-        toRestart = Compass.GetComponent<timer>().unfinCD;
+        resBtnClicked = 0;
+        //resBtnClicked = Compass.GetComponent<timer>().unfinCD;  // For as long as the countdown takes place in timer.cs, I want the variable to restart to be ==1.
+
+        if (Compass.GetComponent<compassRotation>().goalReached == 1)
+        {
+            resButObject.SetActive(true);
+        }
+        
     }
+
 
     void TaskOnClick(){
         //Debug.Log("You have clicked the button!");
-        toRestart = 1;
+        resBtnClicked = 1;
+        resButObject.SetActive(false);
+        //restartButton.enabled = false;
+        //restartButton.interactable = false;
     }
+
 }
+
+
