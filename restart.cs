5c652121769f600e8b05d526bf0cb2cbbd66207a
Mikepag <p14pagk@ionio.gr>
2019-02-25 22:03:11 +0200

    Update restart.cs
    * Added the public Text "roundText" to store the number of the current round and print it on the Canvas in the place where the AngleText used to be.
    * I set the restart button's text to:
       * "Start" before starting the application.
       * "Start next round" after finishing a round.
       * "Quit" after the final (10th) round has finished.
    * If the goal is reached the roundText's colour becomes green.
    
    ___
    --> Works perfectly.
    --> I just need to remove old code and add comments.

--- restart.cs
+++ restart.cs
@@ -8,6 +8,8 @@ public class restart : MonoBehaviour {
     public GameObject Compass;
     public GameObject resButObject; // I use this to hide the restart button when it is clicked.
     public Button restartButton;    // I use this to know when the restart button gets clicked.
+    public Text roundText;  // Declaring a public Text called roundText
+
     public int resBtnClicked;
     public int roundNum;
 
@@ -16,6 +18,7 @@ public class restart : MonoBehaviour {
         restartButton.onClick.AddListener(TaskOnClick); // Whenever the restart button is clicked, call the TaskOnClick() function.
         resButObject.SetActive(true);
         roundNum = 0;
+        roundText.color = Color.black;
     }
 
     void Update()
@@ -23,11 +26,27 @@ public class restart : MonoBehaviour {
         resBtnClicked = 0;
         //resBtnClicked = Compass.GetComponent<timer>().unfinCD;  // For as long as the countdown takes place in timer.cs, I want the variable to restart to be ==1.
 
+        roundText.color = Color.black;
+        roundText.text = "Round: " + roundNum.ToString() + " /10";
+
+        if (roundNum == 0)
+        {
+            resButObject.GetComponentInChildren<Text>().text = "Start!";
+        }
+        else
+        {
+            resButObject.GetComponentInChildren<Text>().text = "Start next round";
+
+        }
+
+
         if (Compass.GetComponent<compassRotation>().goalReached == 1)  // If the goal had been reached and round 10 hasn't started yet...
         {
             resButObject.SetActive(true);   // Show the restart button to start the next round.
+            roundText.color = Color.green; // If the goal is reached for this round, set roundText colour to green.
 
-            if (roundNum == 1)
+
+            if (roundNum == 10)
             {
                 resButObject.GetComponentInChildren<Text>().text = "Quit";
             }
@@ -38,7 +57,7 @@ public class restart : MonoBehaviour {
 
     void TaskOnClick(){
 
-        if (roundNum == 1)
+        if (roundNum == 10)
         {
             UnityEditor.EditorApplication.isPlaying = false;
             Application.Quit();
