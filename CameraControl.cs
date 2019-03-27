9660db1f6763a5b3069c2e6df5e488393d82e2c7
Mikepag <p14pagk@ionio.gr>
2019-03-27 15:32:51 +0200

    Update CameraControl.cs
    * Using 4 variables as boundaries to determine the acceptable input values.
    * Renamed "recentCounter" variable to "arrIn" as it is equal to the current array's cell into which I can put data.
    * Created the variable "arrSize"  which is equal to the array's size (number of not empty cells). Used for calculating average value.
    * Added some comments.
    * Using arrSize (instead of recentCounter) to calculate the recentAverage. (By using recentCounter the recentAverage's value was not the average value of ALL array's cells).
    * Moving anolng the X-Axis again (instead of the Z-Axis). That is because I want the camera to move left-to-right and right-to-left instead of backwards and forwards.
    
    ___
    --> It works quite good but ONLY with the left sensors.
    --> The right sensors return negative values and for some reason the script can not get negative values. Had the same problem in the past. I have to convert all values before sending them to Unity.
    --> Many changes to do here too.

--- CameraControl.cs
+++ CameraControl.cs
@@ -9,22 +9,26 @@ using System.IO.Ports;
 
 public class UltraSonicCameraControl : MonoBehaviour {
 
-
     float posX = 0;
     float posY = 0;
     float posZ = 0;
 
-    float minDistance = 2;
-    float maxDistance = 32;
+    int minLeftDistance = -32;    // Used for setting boundaries for the Input value.
+    int maxLeftDistance = -2;
+    int minRightDistance = 2;
+    int maxRightDistance = 32;
 
-    bool goForward = false;
-    bool goBack = false;
+    bool goForward = false; // Flag to move (rotate) forward (to-the-left).
+    bool goBack = false;    // Flag to move (rotate) backward (to-the-right).
 
-    int[] recentValues = new int[10];
-    public int recentAverage = 0;
-    int recentCounter = 0;
+    public int tempInput;   // Variable that gets input from Arduino.
 
-    public int tempInput;
+    int[] recentValues = new int[10];   // (Circular) Array containing the ten most recent input values.
+    public int recentAverage = 0;   // Average value of all array's values.
+    int arrIn = 0;  // Is equal to the number of the array's cell into which we can insert data. When the array is full, the oldest value gets overwritten.
+    int arrSize = 0;    // Is equal to the array's size (number of not empty cells). Used for calculating average value.
+
+    
 
     SerialPort sp = new SerialPort("COM3", 9600);
     //System.IO.Ports.SerialPort stream = new System.IO.Ports.SerialPort("COM7", 9600); 
@@ -45,54 +49,68 @@ public class UltraSonicCameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        checkMotion();
+        CheckMotion();
     }
 
-    void checkMotion()
+    void CheckMotion()
     {
-        if (recentCounter > 9)
-            recentCounter = 0;
+
+            
 
         //float tempInput = float.Parse(stream.ReadLine());
-        
+
         //if (sp.IsOpen)
         //{
-            tempInput = sp.ReadByte();
-        //}
+        tempInput = sp.ReadByte();  // Get input from Serial Port.
+                                    //}
 
-        if (tempInput >= minDistance && tempInput <= maxDistance)
-        {
-            recentValues[recentCounter] = tempInput;
-            getDirection();
+        //if (tempInput >= minDistance && tempInput <= maxDistance)
+        //if((tempInput >= minLeftDistance && tempInput <= maxLeftDistance) || (tempInput >= minRightDistance && tempInput <= maxRightDistance))  // If the input value is between the boundaries...
+        //if ((tempInput >= minLeftDistance && tempInput <= maxLeftDistance) || (tempInput >= minRightDistance && tempInput <= maxRightDistance))  // If the input value is between the boundaries...
+        if ((tempInput >= -32 && tempInput <= -2) || (tempInput >= 2 && tempInput <= 32))  // If the input value is between the boundaries...
+            {
+                recentValues[arrIn] = tempInput;    //...Add the input to the array.
+            
+            if (arrSize < 10)
+            {
+                arrSize++;
+            }
 
-            recentCounter++;
+            GetDirection();
+
+            arrIn++;    // Increment array's counter.
+            if (arrIn > 9)  // If the end of the array is reached...
+            {
+                arrIn = 0;  //...Go to the first cell again.
+            }
         }
-        else
+        else   // Else, if the input is out of bounds...
         {
-            DeleteRecentValues();
+            DeleteRecentValues();   //... Call DeleteRecentValues() to delete all array's values.
         }
     }
 
 
-    void getDirection()
+    void GetDirection()
     {
         //for (int i = 0; i < recentValues.Length; i++)
         recentAverage = 0;
-        for (int i = 0; i <= recentCounter; i++)
+        //for (int i = 0; i <= arrIn; i++)
+        for(int i = 0; i<arrSize; i++)
         {
             recentAverage += recentValues[i];
         }
 
-        recentAverage = recentAverage / (recentCounter+1);
+        recentAverage = recentAverage / arrSize;
 
-        if (recentValues[recentCounter] > recentAverage)
+        if (recentValues[arrIn] > recentAverage)
         {
             Debug.Log("Back");
             goBack = true;
             goForward = false;
         }
 
-        if (recentValues[recentCounter] < recentAverage)
+        if (recentValues[arrIn] < recentAverage)
         {
             Debug.Log("Forward");
             goForward = true;
@@ -100,24 +118,25 @@ public class UltraSonicCameraControl : MonoBehaviour {
         }
 
         if (goForward)
-            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+            this.transform.position = new Vector3(this.transform.position.x+1, this.transform.position.y, this.transform.position.z);
 
         if (goBack)
-            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 1);
+            this.transform.position = new Vector3(this.transform.position.x-1, this.transform.position.y, this.transform.position.z);
 
         if (goForward || goBack)
         {
-            Debug.Log("Recent input: " + recentValues[recentCounter] + " & Recent average: " + recentAverage);
+            Debug.Log("Recent input: " + recentValues[arrIn] + " & Recent average: " + recentAverage);
         }
     }
 
     void DeleteRecentValues()
     {
-        for(int i=0; i<9; i++)
-        {
-            recentValues[i] = 0;
-        }
-        recentCounter = 0;
+        //for(int i=0; i<9; i++)
+        //{
+        //    recentValues[i] = 0;
+        //}
+        arrIn = 0;
+        arrSize = 0;
         recentAverage = 0;
     }
 
