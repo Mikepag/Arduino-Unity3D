10712e66c9faedbf9dcaafbaa99591d9e03daf5e
Mikepag <p14pagk@ionio.gr>
2019-12-16 16:50:34 +0200

    Update diskRotationP1.cs
    * Added and adjusted comments.

--- Project7/Project4.2/diskRotationP1.cs
+++ Project7/Project4.2/diskRotationP1.cs
@@ -18,7 +18,7 @@ public class diskRotationP1 : MonoBehaviour
     private int isStationary;           // ==0 when the disk is Stationary, ==1 when the disk is currently rotating and has not finished its rotation.
     private int angle;                  // Gets the disk's current rotation value of Y Axis (Disk.transform.localRotation.eulerAngles.y).
     private int resBtnClicked;          // Used to save the current value of the resBtnClicked variable from the restartP1.cs script. (resBtnClicked contains the last round's number in which the restart button got clicked).
-    private int previous_resBtnClicked; // Used to save the previous value of the resBtnClicked variable from the restartP1.cs script. (I use it to know when the restart button gets clicked by compairing it to the current resBtnClicked).
+    private int previous_resBtnClicked; // Used to save the previous frame's value of the resBtnClicked variable from the restartP1.cs script. (I use it to know when the restart button gets clicked by compairing it to the current resBtnClicked).
     private int unfinCD;                // Used to save the value of the unfinCD variable from the timer.cs script.
     private int didCntdown;             // Used to save the value of the didCntdown variable from the timer.cs script.
     private int randomAngle;            // Gets a "random" value which is used when rotating the disk each time the restart button is clicked.
@@ -27,13 +27,12 @@ public class diskRotationP1 : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        sp.Open();
-        isStationary = 1;
-        goalReached = 0;
-        previous_resBtnClicked = 0; // The restart button has not been clicked yet.
-        resBtnClicked = 0;          // The restart button has not been clicked yet.
+        sp.Open();                      // Opens a new serial port connection.
+        isStationary = 1;               // The disk is currently stationary
+        goalReached = 0;                // The goal has not been reached.
+        previous_resBtnClicked = 0;     // The restart button has not been clicked in the previous frame.
+        resBtnClicked = 0;              // The restart button has not been clicked yet.
     }
-
 
 
     // Update is called once per frame
@@ -41,17 +40,17 @@ public class diskRotationP1 : MonoBehaviour
     {
         if (sp.IsOpen)
         {
-            angle = (int)Disk.transform.localRotation.eulerAngles.y;     // Get the current Y Axis rotation value.
+            angle = (int)Disk.transform.localRotation.eulerAngles.y;            // Get the current Y Axis rotation value.
 
-            unfinCD = Disk.GetComponent<timerP1>().unfinCD;                // Getting the value of unfinCD from the timer.cs script.
-            didCntdown = Disk.GetComponent<timerP1>().didCntdown;          // Getting the value of didCntdown from the timer.cs script.
+            unfinCD = Disk.GetComponent<timerP1>().unfinCD;                     // Getting the value of unfinCD from the timer.cs script.
+            didCntdown = Disk.GetComponent<timerP1>().didCntdown;               // Getting the value of didCntdown from the timer.cs script.
 
-            previous_resBtnClicked = resBtnClicked; //Save the previous frame's resBtnClicked value.
-            resBtnClicked = Disk.GetComponent<restartP1>().resBtnClicked;  // Getting the current value of resBtnClicked from the restart.cs script.
+            previous_resBtnClicked = resBtnClicked;                             //Save the previous frame's resBtnClicked value.
+            resBtnClicked = Disk.GetComponent<restartP1>().resBtnClicked;       // Getting the current value of resBtnClicked from the restart.cs script.
             //////
 
-            if ((resBtnClicked - previous_resBtnClicked) == 1)                                             // If the restart button has been clicked...
-            {
+            if ((resBtnClicked - previous_resBtnClicked) == 1)
+            {                                                                   // If the restart button has been clicked...
                 randomAngle = Random.Range(0, 180);                             // Returns a random integer number between 0 and 179.
                 if (randomAngle >= 90)
                 {
@@ -61,42 +60,41 @@ public class diskRotationP1 : MonoBehaviour
                 goalReached = 0;                                                // Every time the restart button gets clicked, a new round starts in which the goal has not been reached yet.
             }
 
-            if (isStationary == 1)              // If the disk is not currently rotating...
-            {
-                directionSteps = sp.ReadByte(); // ...Get value from Arduino via Serial Port.
+            if (isStationary == 1)
+            {                                                                   // If the disk is not currently rotating...
+                directionSteps = sp.ReadByte();                                 // ...Get value from Arduino via Serial Port.
             }
 
-            if (goalReached == 0 && unfinCD == 0 && didCntdown == 1)    // If the the goal is Not reached yet AND the countdown is not currently taking place AND there was a countdown already...
-            {
-                StartCoroutine(RotateObject(directionSteps));           // Call RotateObject() to rotate the disk.
+            if (goalReached == 0 && unfinCD == 0 && didCntdown == 1)
+            {                                                                   // If the the goal is Not reached yet AND the countdown is not currently taking place AND there was a countdown already...
+                StartCoroutine(RotateObject(directionSteps));                   // Call RotateObject() to rotate the disk.
             }
         }
     }
 
 
-
     IEnumerator RotateObject(int directionSteps)
     {
-        if (directionSteps > 0 && directionSteps <= 10) // If the value sent from the arduino is >0 and <=10 (Right to Left rotation)
-        {
-            timesteps = 10 - directionSteps;            // The bigger the value of directionSteps, the slower the gesture was so I want the disk's rotation to last for a smaller amount of timesteps.
-            directionSteps = (11 - directionSteps) * 7; // The bigger the value of directionSteps, the slower the gesture was so I want the disk to rotate less (That's why I use 11-diractionSteps). The " *7" is there so the total angle is not too small.
+        if (directionSteps > 0 && directionSteps <= 10)
+        {                                                                           // If the value sent from the arduino is >0 and <=10 (Right to Left rotation)
+            timesteps = 10 - directionSteps;                                        // The bigger the value of directionSteps, the slower the gesture was so I want the disk's rotation to last for a smaller amount of timesteps.
+            directionSteps = (11 - directionSteps) * 7;                             // The bigger the value of directionSteps, the slower the gesture was so I want the disk to rotate less (That's why I use 11-diractionSteps). The " *7" is there so the total angle is not too small.
             while (timesteps < 10)
             {
-                isStationary = 0;                                               // Because the disk is currently rotating.
-                transform.Rotate(Vector3.up * directionSteps * Time.deltaTime); // Rotate the disk.
-                yield return new WaitForSeconds(0.05F);                         // Suspends the coroutine execution for the given amount of seconds using scaled time. I use this so the rotation takes some time to complete instead of beeing instant.
+                isStationary = 0;                                                   // Because the disk is currently rotating.
+                transform.Rotate(Vector3.up * directionSteps * Time.deltaTime);     // Rotate the disk.
+                yield return new WaitForSeconds(0.05F);                             // Suspends the coroutine execution for the given amount of seconds using scaled time. I use this so the rotation takes some time to complete instead of beeing instant.
                 timesteps++;
                 if (timesteps >= 10)
                 {
-                    isStationary = 1;   // When timesteps==10, the rotation terminates, so the disk is stationary.
+                    isStationary = 1;                                               // When timesteps==10, the rotation terminates, so the disk is stationary.
                 }
             }
         }
-        else if (directionSteps > 10)   // Else, if the value sent from the arduino is >10 (Left to Right rotation). directionSteps is > 10 because I added "10" to seperate it from the Right to Left rotation, so I have to subtract 10 before using it.
-        {
-            timesteps = 10 - (directionSteps - 10);             // The bigger the value of directionSteps, the slower the gesture was so I want the disk's rotation to last for a smaller amount of timesteps.
-            directionSteps = (11 - (directionSteps - 10)) * 7;  // The bigger the value of directionSteps, the slower the gesture was so I want the disk to rotate less (That's why I use 11-diractionSteps). The " *7" is there so the total angle is not too small.
+        else if (directionSteps > 10)
+        {                                                                           // Else, if the value sent from the arduino is >10 (Left to Right rotation). directionSteps is > 10 because I added "10" to seperate it from the Right to Left rotation, so I have to subtract 10 before using it.
+            timesteps = 10 - (directionSteps - 10);                                 // The bigger the value of directionSteps, the slower the gesture was so I want the disk's rotation to last for a smaller amount of timesteps.
+            directionSteps = (11 - (directionSteps - 10)) * 7;                      // The bigger the value of directionSteps, the slower the gesture was so I want the disk to rotate less (That's why I use 11-diractionSteps). The " *7" is there so the total angle is not too small.
             while (timesteps < 10)
             {
                 isStationary = 0;                                                   // Because the disk is currently rotating.
@@ -105,14 +103,14 @@ public class diskRotationP1 : MonoBehaviour
                 timesteps++;
                 if (timesteps >= 10)
                 {
-                    isStationary = 1;   // When timesteps==10, the rotation terminates, so the disk is stationary.
+                    isStationary = 1;                                               // When timesteps==10, the rotation terminates, so the disk is stationary.
                 }
             }
         }
 
-        if (isStationary == 1 && angle >= 175 && angle <= 185 && ((resBtnClicked - previous_resBtnClicked) == 0))    // If the current angle is between 175 and 185 and the button has not been clicked yet to start the next round...
-        {
-            goalReached = 1;    // ...The goal has been successfully reached (for this round).
+        if (isStationary == 1 && angle >= 175 && angle <= 185 && ((resBtnClicked - previous_resBtnClicked) == 0))
+        {                             // If the current angle is between 175 and 185 and the button has not been clicked yet to start the next round...
+            goalReached = 1;          // ...The goal has been successfully reached (for this round).
         }
     }
 }
