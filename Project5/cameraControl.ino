8f673e57e9d983a3da34a8bfa0a1e2e8b7781af8
Mikepag <p14pagk@ionio.gr>
2022-05-01 18:00:11 +0300

    Update cameraControl.ino
    * With the valuable help of N.S. I added a timeout to the pulsein() function using the optional third statement as follows: "pulseIn(echoPin, HIGH, 2500)", with timout being the number of microseconds to wait for the pulse to start. The default value is 1000000μs  (==1 second). 2500μs is enough for the system to calculate distances up to 30-35cm. This change completely eliminated the issue of arduino malfunctions and lag, possibly caused by one or more sensors waiting for a pulse-in signal, but not receiving one.
    * The distance calculation process inside the distCalc() function is now repeated 10 times producing 10 distance values (instead of just one) which are then processed to extract an average distance value. This change was able to reduce the effect of faulty measurements, or the absence thereof, without significantly affecting the overall time needed for the distance calculation.
    * Minnor changes to comments and minnor bug fixes.
    ___
    --> Smooth operation of the Arduino. The random malfunctions issue has finally been resolved.

--- Project5/cameraControl.ino
+++ Project5/cameraControl.ino
@@ -12,17 +12,17 @@ const int echoRRPin = 13;   // Echo pin of Right Right Ultrasonic Ranging Module
 const int flagLedPin = 2;
 const int flashingLedPin = 3;
 
-int distLLInt;            // Integer value of float distanceLL.
-int distLRInt;            // Integer value of float distanceLR.
-int distRLInt;            // Integer value of float distanceRL.
-int distRRInt;            // Integer value of float distanceRR.
+int distLLInt;              // Integer value of float distanceLL.
+int distLRInt;              // Integer value of float distanceLR.
+int distRLInt;              // Integer value of float distanceRL.
+int distRRInt;              // Integer value of float distanceRR.
 
-int avgDistL = -1;        // Average distance value from the Left Sensors. ==-1 means there were not any distances from Left sensors in this timestep, so could not calculate average distance.
-int avgDistR = -1;        // Average distance value from the Right Sensors. ==-1 means there were not any distances from Right sensors in this timestep, so could not calculate average distance.
+int avgDistL = 0;           // Average distance value from the Left Sensors. ==0 means there were not any distances from Left sensors in this timestep, so could not calculate average distance.
+int avgDistR = 0;           // Average distance value from the Right Sensors. ==0 means there were not any distances from Right sensors in this timestep, so could not calculate average distance.
 
-int sensorSwitch = 0;     // 0== Switch Left Sensors ON, 1== Switch Right Sensors ON.
+int sensorSwitch = 0;       // 0== Switch Left Sensors ON, 1== Switch Right Sensors ON.
 
-int totalAvgDistance = 0; // Value sent to Unity. Takes a value between [-32, +32]. ==0 When something's just in the middle between Left and Right sensors, ==-32 or ==32 when on the left or right end.
+int totalAvgDistance = 0;   // Value sent to Unity. Takes a value between [-32, +32]. ==0 When something's just in the middle between Left and Right sensors, ==-32 or ==32 when on the left or right end.
 
 
 //_________________________SETUP():_________________________
@@ -56,11 +56,11 @@ void loop() {
   }
 
   // Calculating average distances for left and right breadboard.
-  avgDistL = avgDistCalc(distLLInt, distLRInt); // Calling avgDistCalc() to calcualte the average distance for the Left sensors.
-  avgDistR = avgDistCalc(distRLInt, distRRInt); // Calling avgDistCalc() to calcualte the average distance for the Right sensors.
+  avgDistL = avgDistCalc(distLLInt, distLRInt);                 // Calling avgDistCalc() to calcualte the average distance for the Left sensors.
+  avgDistR = avgDistCalc(distRLInt, distRRInt);                 // Calling avgDistCalc() to calcualte the average distance for the Right sensors.
 
   // Calculating total average distance and sending it to Unity.
-  totalAvgDistance = totalAvgDistCalc(avgDistL, avgDistR);  // Calling totalAvgDistCalc() to calculate the total average distance from all four sensors.
+  totalAvgDistance = totalAvgDistCalc(avgDistL, avgDistR);      // Calling totalAvgDistCalc() to calculate the total average distance from all four sensors.
   
   /*// Printing all the distances while debugging.
   Serial.print("LL: ");
@@ -81,11 +81,10 @@ void loop() {
   Serial.print(totalAvgDistance);
   Serial.print("\n\n");
   */
-  
-  
+
   Serial.write(totalAvgDistance); // Send totalAvgDistance to Unity.
   Serial.flush();                 // Wait for the data to be sent successfully to Unity.
-  delay(100);                     // Delay between each time that either left or right sensor are "enabled".
+  delay(50);                      // Delay between each time that either left or right sensor are "enabled".
   
   digitalWrite(flagLedPin, LOW);
 }
@@ -93,53 +92,104 @@ void loop() {
 
 //_________________________FUNCTIONS:_________________________
 int distCalc(int trigPin, int echoPin, int flashingLedPin){ // Gets the sensor's trigger and echo pin and flashingLed's pin, calculates and returns a distance.
-  float duration;                     // Time duration between emiting and receiving signal from the sensor.
-  float distance;                     // Distance calulated based on duration.
-  int distInt;                        // Integer value of float distance.
+  float duration;                                           // Time duration between emiting and receiving signal from the sensor.
+  float distance;                                           // Distance calulated based on duration.
+  int distInt;                                              // Integer value of float distance.
+  int i;
+  int sumDistance=0;                                        // Sum of all distance values between acceptable boundaries (used for calculating mean distance value).
+  int distanceCount=0;                                      // Counter of distance values between acceptable boundaries (used for calculating mean distance value).
+  float meanDistance=0;                                     // Mean value of all acceptable calculated distance values.
+  int meanDistInt=0;                                        // Integer value of the mean value of all acceptable calculated distance values.
 
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);               // Delay, to let sensor switch trigPin to LOW.
-  digitalWrite(flashingLedPin, HIGH);
-  digitalWrite(trigPin, HIGH);        // Emiting signal.
-  delayMicroseconds(10);              // Delay, to let sensor emit signal.
-  digitalWrite(trigPin, LOW);
-  digitalWrite(flashingLedPin, LOW);
-  duration = pulseIn(echoPin, HIGH);  // Receiving signal and saving its duracion.
-  distance = duration/58.2;           // Calculating distance in centimeters, based on the speed of sound.
-  distInt = distance;                 // Converting float distance value to integer.
-  return distInt;                     // Return the calculated distance.
-}
+  for(i=0; i<10; i++){                                      // Repetition of the distance calculation procedure to increase accuracy and decrease sensor errors.
+    digitalWrite(trigPin, LOW);                             // Switching trigPin to LOW.
+    delayMicroseconds(2);                                   // Delay, to let sensor switch trigPin to LOW.
+    digitalWrite(flashingLedPin, HIGH);                     // Switching flashingLED ON during the signal emission.
+    digitalWrite(trigPin, HIGH);                            // Start signal emission.
+    delayMicroseconds(10);                                  // Delay, to let the sensor emit signal.
+    digitalWrite(trigPin, LOW);                             // Stop signal emission.
+    digitalWrite(flashingLedPin, LOW);                      // Switching flashingLED OFF after the signal emission.
+    
+                                                            // Receiving signal and saving its duration in microseconds.
+    duration = pulseIn(echoPin, HIGH, 2500);                // The third statement is timeout: the number of microseconds to wait for the pulse to start; default is 1 second == 1000000μs.
+                                                            // I used 2500μs to resolve the issue of random huge distances calculated by the sensors. 2500μs is enough to measure distances up to 30-35cm.
+    
+    //Serial.print("  Duration: ");
+    //Serial.print(duration);
+    //Serial.print("      ");
+    
+    distance = duration/58.2;                               // Calculating distance in centimeters, based on the speed of sound.
+    distInt = round(distance);                              // Converting float distance value to integer.
+    
+    //Serial.print("  Distance: ");
+    //Serial.print(distInt);
+    //Serial.print("\n");
 
-int avgDistCalc(int distLInt, int distRInt){  // Gets two distance values and calculates and returns their average value.
-  int avgDist = 0;                      // Default value. Switch to this after debugging.
-  //int avgDist = -1;                       // For debugging reasons. It is converted to 0 before calculating totalAvgDistance.
-  
-  if(distLInt <= 30 || distRInt <= 30){   // If at least one of the Sensors of the Breadboard detects something...
-    digitalWrite(flagLedPin, HIGH);
-    if(distLInt <= 30 && distRInt <= 30){ // If both Sensors detect something at the same time...
-      avgDist = (distLInt + distRInt)/2;  //... avgDist = average value of the two distances.
-    }
-    else if(distLInt <= 30){              // Else, if only L Sensor detects something...
-      avgDist = distLInt;                 //... avgDist = distance calculated by L sensor.
-    }
-    else if(distRInt <= 30){              //  Else, if only R Sensor detects something...
-      avgDist = distRInt;                 //... avgDist = distance calculated by R sensor.
+    if(distInt >=2 && distInt<=32){                         // If the last calculated distance value is between the acceptable boundaries...
+      sumDistance += distInt;                               //... Add the value to the sum.
+      distanceCount ++;                                     //... And increase the counter.
     }
   }
-  return avgDist;                         // Return the calculated average distance.
+
+  if(distanceCount>0){                                      // If acceptable distance values exist... 
+    meanDistance = sumDistance/distanceCount;               //... Calculate mean distance value from all acceptable distance values.
+    meanDistInt = round(meanDistance);                      //... Convert float mean distance value to integer.
+    return meanDistInt;                                     //... Return the calculated mean distance.
+  }
+  else{                                                     // Else, if no acceptable distance values were calculated...
+    return 0;                                               //... Return 0 (default distance value).
+  }
+}
+
+/*int avgDistCalc(int distLInt, int distRInt){  // Gets two distance values and calculates and returns their average value.
+  int avgDist = 0;                              // Default value. Switch to this after debugging.
+  //int avgDist = -1;                           // For debugging reasons. It is converted to 0 before calculating totalAvgDistance.
+  
+  if(distLInt <= 30 || distRInt <= 30){         // If at least one of the Sensors of the Breadboard detects something...
+    digitalWrite(flagLedPin, HIGH);
+    if(distLInt <= 30 && distRInt <= 30){       // If both Sensors detect something at the same time...
+      avgDist = (distLInt + distRInt)/2;        //... avgDist = average value of the two distances.
+    }
+    else if(distLInt <= 30){                    // Else, if only L Sensor detects something...
+      avgDist = distLInt;                       //... avgDist = distance calculated by L sensor.
+    }
+    else if(distRInt <= 30){                    //  Else, if only R Sensor detects something...
+      avgDist = distRInt;                       //... avgDist = distance calculated by R sensor.
+    }
+  }
+  return avgDist;                               // Return the calculated average distance.
+}*/
+
+int avgDistCalc(int distLInt, int distRInt){                                          // Gets two distance values and calculates and returns their average value.
+  int avgDist = 0;                                                                    // Default value. Switch to this after debugging.
+  //int avgDist = -1;                                                                 // For debugging reasons. It is converted to 0 before calculating totalAvgDistance.
+  
+  if(((distLInt >=2) && (distLInt <= 32)) || ((distRInt >=2) && (distRInt <= 32))){   // If at least one of the Sensors of the Breadboard detects something...
+    digitalWrite(flagLedPin, HIGH);
+    if(((distLInt >=2) && (distLInt <= 32)) && ((distRInt >=2) && (distRInt <= 32))){ // If both Sensors detect something at the same time...
+      avgDist = (distLInt + distRInt)/2;                                              //... avgDist = average value of the two distances.
+    }
+    else if((distLInt >=2)&&(distLInt <= 32)){                                        // Else, if only L Sensor detects something...
+      avgDist = distLInt;                                                             //... avgDist = distance calculated by L sensor.
+    }
+    else if((distRInt >=2)&&(distRInt <= 32)){                                        // Else, if only R Sensor detects something...
+      avgDist = distRInt;                                                             //... avgDist = distance calculated by R sensor.
+    }
+  }
+  return avgDist;                                                                     // Return the calculated average distance.
 }
 
 int totalAvgDistCalc(int avgDistL, int avgDistR){ // Gets the left and right average distances and calculates and returns the position of what is beeing detected by the sensors.
   int totalAvgDistance = 0;
 
-  //if(avgDistL == -1){                     //For debugging reasons. Remove it afterwards.
+  //if(avgDistL == -1){ //For debugging reasons. Remove it afterwards.
   //  avgDistL = 0;
   //}
-  //if(avgDistR == -1){                     //For debugging reasons. Remove it afterwards.
+  //if(avgDistR == -1){ //For debugging reasons. Remove it afterwards.
   //  avgDistR = 0;
   //}
   
-  totalAvgDistance = avgDistL - avgDistR; // Position is equal to avgL - avgR. It takes values between in [-32,32]
-  totalAvgDistance += 32;                 // I want it to take values in [0,64] because negative values can not be sent to Unity.
-  return totalAvgDistance;                // Return the calculated total average distance from the middle of the two breadboards.
+  totalAvgDistance = avgDistL - avgDistR;         // Position is equal to avgL - avgR. It takes values between in [-32,32]
+  totalAvgDistance += 32;                         // I want it to take values in [0,64] because negative values can not be sent to Unity.
+  return totalAvgDistance;                        // Return the calculated total average distance from the middle of the two breadboards.
 }
