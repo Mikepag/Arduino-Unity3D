8839b7d57c8145f4f86d92e8b2a165015d3038c4
Mikepag <p14pagk@ionio.gr>
2022-11-08 19:42:48 +0200

    Update ghostRotationDamped.cs
    * Calculating the damped rotation duration (dampedRotationDuration) based on the integer absolute value of initialAngleOfRotation each time a new damped rotation starts, instead of having a constant duration.
    * Setting very big duration values to maxDampedRotationDuration in order to avoid the object rotating for too much time.
    * Added missing comments.
    ___
    --> Remains operational, feels very natural and after the last changes (regarding the rotation duration) the accuracy of very slow gestures has been increased.
    --> Its usability has to be tested before replacing ghostRotation.cs.

--- Project7/Pepper's_Ghost/ghostRotationDamped.cs
+++ Project7/Pepper's_Ghost/ghostRotationDamped.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO.Ports;
 using UnityEngine.UI;
 using System.IO;
+using System;
 using UnityEngine.SceneManagement;
 
 public class ghostRotationDamped : MonoBehaviour
@@ -14,25 +15,26 @@ public class ghostRotationDamped : MonoBehaviour
     //Recieving data from the arduino:
     SerialPort sp = new SerialPort("COM3", 9600);
 
-    private const int MinLeftDistance = -32;    // Used for setting boundaries for the Input value.
-    private const int MaxLeftDistance = -2;     // Used for setting boundaries for the Input value.
-    private const int MinRightDistance = 2;     // Used for setting boundaries for the Input value.
-    private const int MaxRightDistance = 32;    // Used for setting boundaries for the Input value.
-    private const int MaxAOR = 25;              // MaxAngleOfRotation. Maximum degrees angle the Ghost can be rotated.
-    private const int SIZE = 15;                // previousPositions[] size.
+    private const int MinLeftDistance = -32;            // Used for setting boundaries for the Input value.
+    private const int MaxLeftDistance = -2;             // Used for setting boundaries for the Input value.
+    private const int MinRightDistance = 2;             // Used for setting boundaries for the Input value.
+    private const int MaxRightDistance = 32;            // Used for setting boundaries for the Input value.
+    private const int MaxAOR = 25;                      // MaxAngleOfRotation. Maximum degrees angle the Ghost can be rotated.
+    private const int SIZE = 15;                        // previousPositions[] size.
+    private const int maxDampedRotationDuration = 20;   // Maximum number of timesteps the Ghost's damped rotation can last.
 
-    private int timestepsSinceOutOfBounds = 0;  // Used to eliminate the possibility of a hand gesture not being recognised due to Arduino malfanction.
+    private int timestepsSinceOutOfBounds = 0;          // Counter of the elapsed timesteps since the hand position went out of the predefined boundaries.
 
-    public float angleOfRotation;                 // Contains the value in which the Ghost will be rotated.
-    public int currentPosition;                 // Variable that gets input from Arduino.
+    public float angleOfRotation;                       // Contains the value in which the Ghost will be rotated.
+    public int currentPosition;                         // Variable that gets input from Arduino.
 
-    private int[] previousPositions = new int[SIZE]; // (Circular) Array containing the [SIZE] most recent input values.
-    public int averagePreviousPosition;                   // Average value of all array's values.
-    private int arrIn;                          // Is equal to the number of the array's cell into which we can insert data. When the array is full, the oldest value gets overwritten.
-    private int currentArrSize;                 // Is equal to the array's current size (number of not empty cells). Used for calculating average value.
+    private int[] previousPositions = new int[SIZE];    // (Circular) Array containing the [SIZE] most recent input values.
+    public int averagePreviousPosition;                 // Average value of all array's values.
+    private int arrIn;                                  // Is equal to the number of the array's cell into which we can insert data. When the array is full, the oldest value gets overwritten.
+    private int currentArrSize;                         // Is equal to the array's current size (number of not empty cells). Used for calculating average value.
 
-    public float initialAngleOfDampedRotation;
-
+    public float initialAngleOfDampedRotation;          // Gets the last angleOfRotation value before the hand position went out of the predefined boundaries. Used as the initial speed for the damped rotation.
+    private int dampedRotationDuration;                 // Duration of the damped rotation in timesteps.
 
 
     //__________________________________________________START():__________________________________________________
@@ -44,6 +46,7 @@ public class ghostRotationDamped : MonoBehaviour
         averagePreviousPosition = 0;
         arrIn = 0;
         currentArrSize = 0;
+        dampedRotationDuration = 0; 
     }
 
 
@@ -51,51 +54,56 @@ public class ghostRotationDamped : MonoBehaviour
     void Update()   // Update is called once per frame
     {
         if (Input.GetKeyDown(KeyCode.Backspace))
-        {                                                               // Whenever the Backspace key is clicked...
-            SceneManager.LoadScene("Assets/Scenes/Menu.unity");         // Load Menu Scene.
+        {                                                       // Whenever the Backspace key is clicked...
+            SceneManager.LoadScene("Assets/Scenes/Menu.unity"); // Load Menu Scene.
         }
 
-        currentPosition = sp.ReadByte();                                      // Get input from Serial Port.
-        currentPosition -= 32;                                                // I added 32 before sending it here, so I have to subtract 32 now to get the real value.
+        currentPosition = sp.ReadByte();                        // Get input from Serial Port.
+        currentPosition -= 32;                                  // I added 32 before sending it here, so I have to subtract 32 now to get the real value.
 
         if ((currentPosition >= MinLeftDistance && currentPosition <= MaxLeftDistance) || (currentPosition >= MinRightDistance && currentPosition <= MaxRightDistance))
-        {                                                               // If the input value is between the boundaries...
+        {                                                       // If the input value is between the boundaries...
             timestepsSinceOutOfBounds = 0;
 
-            previousPositions[arrIn] = currentPosition;                            //...Add the input to the array.
+            previousPositions[arrIn] = currentPosition;         //...Add the input to the array.
 
             if (currentArrSize < SIZE)
             {
-                currentArrSize++;                                       // Increment currentArrSize every time a new value gets added, until currentArrSize == SIZE.
+                currentArrSize++;                               // Increment currentArrSize every time a new value gets added, until currentArrSize == SIZE.
             }
             
-            angleOfRotation = GetAngleOfRotation();
-            GhostRotation(angleOfRotation);                                             // Call GhostRotation() to rotate the Ghost.
+            angleOfRotation = GetAngleOfRotation();             // Call GetAngleOfRotation() to get the desired angle (and direction) of the rotation.
+            GhostRotation(angleOfRotation);                     // Call GhostRotation() to rotate the Ghost.
             
-            arrIn++;                                                    // Increment array's counter.
+            arrIn++;                                            // Increment array's counter.
             if (arrIn > (SIZE - 1))
-            {                                                           // If the end of the array is reached...
-                arrIn = 0;                                              //..."Point" to the first cell again.
+            {                                                   // If the end of the array is reached...
+                arrIn = 0;                                      //..."Point" to the first cell again.
             }
         }
-        else                                                            // Else, if the input is out of bounds...
+        else                                                                                    // Else, if the input is out of bounds...
         {
-            if (timestepsSinceOutOfBounds == 0) // If it is the first time the currentPosition is out of bounds...
+            if (timestepsSinceOutOfBounds == 0)                                                 // If it is the first time the currentPosition is out of bounds...
             {
-                initialAngleOfDampedRotation = angleOfRotation;
+                initialAngleOfDampedRotation = angleOfRotation;                                 // Save the last calculated angleOfRotation to be used as the initialAngleofDampedRotation.
+                dampedRotationDuration = (int)(Math.Abs(initialAngleOfDampedRotation));         // Geting the integer absolute value of initialAngleOfRotation. 
+                if (dampedRotationDuration > maxDampedRotationDuration)
+                {
+                    dampedRotationDuration = maxDampedRotationDuration;                         // If the calculated duration is longer than the predefined maximum duration, set it to the maximum value.
+                }
             }
 
-            if (timestepsSinceOutOfBounds < 20)
-            {
-                DampedGhostRotation(initialAngleOfDampedRotation, timestepsSinceOutOfBounds);
+            if (timestepsSinceOutOfBounds < dampedRotationDuration)                             // While the timestepsSinceOutOfBounds haven't reached the maximum rotationDuration value...
+            {   
+                DampedGhostRotation(initialAngleOfDampedRotation, timestepsSinceOutOfBounds);   //...Call DampedGhostRotation() to rotate the Ghost.
             }
             
-            if (timestepsSinceOutOfBounds >= 2)     // If 2 timesteps have passed since the time in which the input value went out of bounds...
+            if (timestepsSinceOutOfBounds >= 2)                                                 // If 2 timesteps have passed since the time in which the input value went out of bounds...
             {
-                DeletePreviousPositions();                                   //...Call DeletePreviousPositions() to delete all array's values.
+                DeletePreviousPositions();                                                      //...Call DeletePreviousPositions() to delete all array's values.
             }
 
-            timestepsSinceOutOfBounds++;                                // Increment the timesteps since the time in which the input value went out of bounds.
+            timestepsSinceOutOfBounds++;                                                        // Increment the timesteps since the time in which the input value went out of bounds.
         }
     }
 
@@ -107,9 +115,9 @@ public class ghostRotationDamped : MonoBehaviour
     float GetAngleOfRotation()
     {
         averagePreviousPosition = 0;
-        if (currentArrSize > 0) // To avoid the division with zero when the array is empty.
+        if (currentArrSize > 0) // To avoid the division with zero when the array is empty. (Not really needed since the function is only called if there is at least one value in the array.)
         {
-            for (int i = 0; i < currentArrSize; i++)                        // Calculating average value for all array's "not-empty" cells:
+            for (int i = 0; i < currentArrSize; i++)    // Calculating average value for all array's "not-empty" cells:
             {
                 averagePreviousPosition += previousPositions[i];
             }
@@ -125,27 +133,30 @@ public class ghostRotationDamped : MonoBehaviour
 
 
     //__________________________________________________GHOSTROTATION():__________________________________________________
-    void GhostRotation(float angleOfRotation)                                                 // Call GhostRotation() to rotate the Ghost.
+    void GhostRotation(float angleOfRotation)   // Call GhostRotation() to rotate the Ghost while the hand position is between the boundaries.
     {
         // --- GHOST ROTATION ---
-        Ghost.transform.Rotate(0, 0, angleOfRotation);                     // Rotate the Ghost in the Z-Axis in the direction and degrees provided by angleOfRotation.
+        Ghost.transform.Rotate(0, 0, angleOfRotation);  // Rotates the Ghost in the Z-Axis in the direction and degrees provided by angleOfRotation.
     }
 
 
     //__________________________________________________DAMPEDGHOSTROTATION():__________________________________________________
-    void DampedGhostRotation(float initialAngleOfDampedRotation, int timestepsSinceOutOfBounds)
+    void DampedGhostRotation(float initialAngleOfDampedRotation, int timestepsSinceOutOfBounds) // Call DampedGhostRotation() to smoothly stop (dampen) the rotating Ghost after the hand position stopped being between the boundaries.
     {
-        angleOfRotation = initialAngleOfDampedRotation * (100 - (5 * (1 + timestepsSinceOutOfBounds))) / 100;   // Remove 5% of the initial angle of rotation for every elapsed timestep, so the rotational speed decreases.
+        // Automatically calculates the angleOfRotation's decreasing rate as a percentage of the initial angle of rotation to control how long the rotation is going to last.
+        // aOR = iAODR * (100 - (100 * ts/dur))/100, OR: aOR = iAODR * (1 - ts/dur), OR: aOR = iAODR * X%, where X is the appropriate percentage (e.g: aOR = iAODR * 50%, aOR = iAODR * 95% etc.).
+        // Removes the appropriate percentage from the initial angle of rotation for every elapsed timestep, so the rotational speed decreases and reaches 0 (0% of the iAODR) when the maximum rotation's duration is reached:
+        angleOfRotation = initialAngleOfDampedRotation * (100 - (100/dampedRotationDuration * (1 + timestepsSinceOutOfBounds))) / 100;
 
         // --- GHOST ROTATION ---
-        Ghost.transform.Rotate(0, 0, angleOfRotation);
+        Ghost.transform.Rotate(0, 0, angleOfRotation);  // Rotates the Ghost in the Z-Axis in the direction and degrees provided by angleOfRotation.
     }
 
 
     //__________________________________________________DELETEPREVIOUSPOSITIONS():__________________________________________________
-    void DeletePreviousPositions()                                           // Function used for "deleting" all recent values from the array.
+    void DeletePreviousPositions()  // Function used for "deleting" all recent values from the array.
     {
-        arrIn = 0;                                                      // Put the next input value to the first array's cell.
-        currentArrSize = 0;                                             // Array's size == 0 means that array is currently empty.
+        arrIn = 0;                  // Put the next input value to the first array's cell.
+        currentArrSize = 0;         // Array's size == 0 means that array is currently empty.
     }
 }
